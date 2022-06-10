83971d8a8e25ce2bb0aa6118572ed5abd2180086
thiago Celso Guimarães e Souza <thiago.celso@ieadpe.org.br>
2022-06-10 08:58:39 -0300

    Exemplos Java Script

---
+++ ternario.js
@@ -0,0 +1,15 @@
+const idadeMinima = 18; 
+const idadeCliente = 19; 
+
+if (idadeCliente>=idadeMinima){
+    console.log("Pode Cerveja!")
+}else{
+    console.log("Não pode Cerveja!")
+}
+
+//Operador ternário (condição ? verdadeiro : falso)
+console.log(idadeCliente >= idadeMinima ? "Cerveja!":"Suco!"); 
+
+//PorqueÊ Operdor terenário? São três operadoder em uma única linha (ternário) 
+
+
